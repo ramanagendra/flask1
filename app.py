6b0aae5233989214f108a53657d70a76334fef32
I RAMANAGENDRA <ramanagendra221@gmail.com>
2024-12-11 14:28:11 +0530

    hi

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-# creating flask application 
-
-from flask import Flask, render_template
+# creating flask ap
 
 ## create the flask app
 app=Flask(__name__)
@@ -25,29 +23,7 @@ def success(score):
 def fail(score):
     return "<h1>the person is failed and score is<h1> "+str(score)
 
-@app.route('/calculate',methods=['POST','GET'])
-def calculate():
-    if request.method=='GET':
-        return render_template('calculate.html')
-    else:
-        maths=float(request.form['maths'])
-        science=float(request.form['science'])
-        history=float(request.form['history'])
 
-        average_marks=(maths+science+history)/3
-        result="" 
-        if average_marks>=50:
-            result="success"
-        else:
-            result="fail"
-
-        #return redirect(url_for(result,score=average_marks))
-
-
-        return render_template('result.html',results=average_marks)
-
-
-## Assignemnt Try for loop
 
 if __name__=='__main__':
     app.run(debug=True) 
